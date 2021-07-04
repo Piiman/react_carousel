9a773b9208ef39fe080b4b0b3d014515b39efcdb
Piiman <palomar.miguel2d@gmail.com>
2021-07-04 00:47:31 -0500

    updated tab buttons

--- src/carousel.js
+++ src/carousel.js
@@ -8,7 +8,7 @@ const Carousel = ({data}) => {
 
     const nextSlot = () => {
         setCurrent(current === length - 1 ? 0 : current + 1);
-      };
+    };
     
     const prevSlot = () => {
       setCurrent(current === 0 ? length - 1 : current - 1);
@@ -25,16 +25,18 @@ const Carousel = ({data}) => {
                     return (
                         <div key={index}>
                             {index === current && (
-                            <Slot image={slot.image} title={slot.title} text={slot.text}/>
+                            <Slot image={slot.image_banner} title={slot.name} text={slot.text}/>
                             )}
                         </div>
                     )
                 })}
             </div>
             <div className="tabs">
-                <span className={current == 0 ? "tab active" : "tab"} onClick={() => setCurrent(0)}></span>
-                <span className={current == 1 ? "tab active" : "tab"} onClick={() => setCurrent(1)}></span>
-                <span className={current == 2 ? "tab active" : "tab"} onClick={() => setCurrent(2)}></span>
+                {
+                    data.map((slot,index) => {
+                        return <span className={current === index ? "tab active" : "tab"} onClick={() => setCurrent(index)}></span>  
+                    })
+                }
             </div>
             <div className="next" onClick={nextSlot}></div>
             <div className="prev" onClick={prevSlot}></div>
